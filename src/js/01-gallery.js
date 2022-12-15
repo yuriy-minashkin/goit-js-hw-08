249ea6fc017632cfb06ac91dab8d7f9df1f5338c
Yuriy Minashkin <y.minashkin@gmail.com>
2022-12-15 13:12:08 +0200

    json updated

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,18 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+  galleryEl.insertAdjacentHTML('beforeend',
+    `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+    </a>`)
+})
+
+var lightbox = new SimpleLightbox('.gallery a',
+  { captionsData: 'alt', captionPosition: 'bottom', captionDelay: '250' });
