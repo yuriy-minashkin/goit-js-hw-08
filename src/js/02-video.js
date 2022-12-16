ccb15dd31b5ee21955cdff6d2f628cf07739d8bc
Yuriy Minashkin <y.minashkin@gmail.com>
2022-12-16 20:52:10 +0200

    Final changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlayback, 1000));
+
+function onPlayback(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+const outputStorageTimeParsed = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+player.setCurrentTime(outputStorageTimeParsed);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+var throttle = require('lodash.throttle');
 
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onInput, 500));
+
+returnMessageToInput();
+
+function onInput() {
+  const formData = new FormData(formEl);
+  const inputObj = {};
+  for (let [email, message] of formData) {
+    inputObj[email] = message;
+  }
+
+  const userData = JSON.stringify(inputObj);
+  localStorage.setItem('feedback-form-state', userData);
+}
+
+function returnMessageToInput() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+  const userDataRecall = JSON.parse(savedMessage);
+  if (savedMessage) {
+    formEl[0].value = userDataRecall.email;
+    formEl[1].value = userDataRecall.message;
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  const userDataoutput = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  console.log(userDataoutput);
+  localStorage.removeItem('feedback-form-state');
+}
